63f0f9f189a5e4b40c4dd20295cd4b6c950cfcf6
Sushant Mimani <sushant.mimani@gmail.com>
2017-10-21 03:30:12 -0400

    Display list and organism name

--- front-end/public/views/index.controller.js
+++ front-end/public/views/index.controller.js
@@ -13,6 +13,7 @@
       model.prev = "";
       model.searchDNA = searchDNA;
       model.clearHistory = clearHistory;
+      model.clearConsole = clearConsole;
 
 
       function clearHistory() {
@@ -25,7 +26,7 @@
 
             model.DNAList=$localStorage.result;
             for(let index in $localStorage.result)
-              model.prev+= "<br/>"+index+"---->"+$localStorage.result[index];
+              model.prev+= "<br/>"+index+":"+$localStorage.result[index];
           }
 
           console.log(model.DNAList)
@@ -33,18 +34,26 @@
       init();
       function searchDNA() {
           let temp = model.dna;
+          model.message+= "<br/>"+temp+":Searching";
+
           IndexService.searchDNA(model.dna)
               .then(function (response) {
                   console.log(response.data);
-                  model.message+= "<br/>"+temp+"------>"+response.data;
-                  model.DNAList[temp]=response.data;
+                  if(response.data.list==="Unavailable"){
+                      model.message+= "<br/>"+temp+":Not found";
+                      model.DNAList[temp]="Unavailable";
+
+                  } else {
+                      model.message+= "<br/>"+temp+":"+JSON.stringify(response.data);
+                      model.DNAList[temp]=JSON.stringify(response.data);
+                  }
                   console.log(model.DNAList);
                   $localStorage.result = model.DNAList;
               })
           model.dna="";
       }
 
-      model.clearConsole = function(){
+      function clearConsole(){
         model.message = "DNA Sequencing";
       }
 
