2bea4b34a8e6ae7450813bcee89041a0c3cb73f9
Sushant Mimani <sushant.mimani@gmail.com>
2017-10-22 14:21:35 -0400

    Trap control+c and terminate service

--- run.bash
+++ run.bash
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+control_c()
+# run if user hits control-c
+{
+  echo -en "\nStopping the Web Server and the REST Service\n"
+  kill -9 $(lsof -t -i:5000)
+  exit
+}
+
+trap control_c SIGINT
+
 cd service
-mkdir lists
+mkdir -p lists
 pip install --user -r requirements.txt
 ./service.py &
 cd ../front-end
 npm install && npm start
+
+
+
+
+
+
